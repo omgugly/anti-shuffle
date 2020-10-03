acda777719938b6bf26df711b1ec348783ba3c17
Hao <xvender7@gmail.com>
2020-10-03 12:53:46 +0300

    Clean up, args removal from seats

--- server/server_main.lua
+++ server/server_main.lua
@@ -1,18 +1,15 @@
 Citizen.CreateThread(function()
-	RegisterCommand('antishuffle', function(source, args, rawCommand)
-		local _source = source
-		TriggerClientEvent('omgugly:toggleShuffle', _source)
+	RegisterCommand('antishuffle', function(source, rawCommand)
+		TriggerClientEvent('omgugly:toggleShuffle', source)
 	end)
 
-	RegisterCommand('entryslide', function(source, args, rawCommand)
-		local _source = source
-		TriggerClientEvent('omgugly:toggleSlide', _source)
+	RegisterCommand('entryslide', function(source, rawCommand)
+		TriggerClientEvent('omgugly:toggleSlide', source)
 	end)
 	
 	if (enableSeatCommand) then
-		RegisterCommand('seat', function(source, args, rawCommand)
-			local _source = source
-			TriggerClientEvent('omgugly:seat', _source, args[1])
+		RegisterCommand('seat', function(source, rawCommand)
+			TriggerClientEvent('omgugly:seat', source)
 		end)
 	end
 end)
